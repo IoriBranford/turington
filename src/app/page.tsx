2afa83c52ec63459693bca2fa08aae0b6a1835f7
IoriBranford <ioribranford@gmail.com>
2023-06-19 18:59:30 -0700

    Show bot.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,20 @@ import { useState } from "react";
 import { Share_Tech_Mono } from "next/font/google";
 import { AiDetectInput, AiDetectOutput } from "./api/aidetect/route";
 import useSWRMutation from "swr/mutation";
+import Image from "next/image";
 
 const AiFont = Share_Tech_Mono({ weight: "400", subsets: ["latin"] });
 
+type AiMood =
+  | "friendly"
+  | "neutral"
+  | "suspicious"
+  | "annoyed"
+  | "hostile"
+  | "dead";
+
 export default function Chat() {
+  const [aiMood, setAiMood] = useState<AiMood>("friendly");
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const [lastInput, setLastInput] = useState("");
 
@@ -36,9 +46,11 @@ export default function Chat() {
   const lastAiMessage = messages.findLast((m) => m.role !== "user");
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch select-none">
+    <div className="flex flex-col w-full max-w-xl py-24 mx-auto stretch select-none">
+      <img className="fixed self-center" src={`ai-${aiMood}.svg`} alt={`ai-${aiMood}.svg`} />
+
       {lastAiMessage ? (
-        <div className="fixed top-0 w-full max-w-md p-2 mt-8 border border-gray-300 rounded shadow-xl ">
+        <div className="fixed top-0 w-full max-w-xl p-2 mt-8 border border-gray-300 rounded shadow-xl bg-white bg-opacity-75">
           <div className={AiFont.className}>{lastAiMessage.content}</div>
         </div>
       ) : null}
@@ -51,7 +63,7 @@ export default function Chat() {
         }}
       >
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="fixed bottom-0 w-full max-w-xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder={lastInput}
           onChange={handleInputChange}
@@ -60,7 +72,7 @@ export default function Chat() {
         />
       </form>
 
-      <div className="fixed bottom-16 w-full max-w-xs p-2 mb-8 border border-gray-300 rounded shadow-xl">
+      <div className="fixed bottom-16 w-full max-w-fit p-2 mb-8 border border-gray-300 rounded shadow-xl">
         {isMutating
           ? "Analyzing..."
           : error
@@ -71,15 +83,15 @@ export default function Chat() {
           ? aiDetectData.error.msg
           : aiDetectData.score
           ? `${Math.floor(aiDetectData.score * 100)}% AI`
-          // : aiDetectData.errors
-          // ? aiDetectData.errors.map((e) => e.description).join("\n")
-          // : aiDetectData.scores
-          // ? aiDetectData.scores
-          //     .map(
-          //       (score) => `${Math.floor(score.score * 100)}% ${score.label}`
-          //     )
-          //     .join(", ")
-          : "No scores"}
+          : // : aiDetectData.errors
+            // ? aiDetectData.errors.map((e) => e.description).join("\n")
+            // : aiDetectData.scores
+            // ? aiDetectData.scores
+            //     .map(
+            //       (score) => `${Math.floor(score.score * 100)}% ${score.label}`
+            //     )
+            //     .join(", ")
+            "No scores"}
       </div>
     </div>
   );
