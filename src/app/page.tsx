57c98876ac304b3be9777f64d731a9ac23986006
IoriBranford <ioribranford@gmail.com>
2023-06-20 21:36:16 -0700

    Implement robot mood bar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { AiDetectInput, AiDetectOutput } from "./api/aidetect/route";
 import useSWRMutation from "swr/mutation";
 import AiSprite from "../components/AiSprite";
 import ChatMessageBox from "../components/ChatMessageBox";
+import AiMoodBar from "../components/AiMoodBar";
 
 const AiFont = Share_Tech_Mono({ weight: "400", subsets: ["latin"] });
 
@@ -79,8 +80,8 @@ export default function Chat() {
         />
       </form>
 
-      <div className="fixed bottom-16 w-full max-w-fit p-2 mb-8 border border-gray-300 rounded shadow-xl">
-        {aiMood}
+      <div className="fixed bottom-16 w-full max-w-xl p-2 mb-8 border border-gray-300 rounded shadow-xl">
+        <AiMoodBar mood={aiMood} />
       </div>
     </div>
   );
---
+++ src/components/AiMoodBar.tsx
@@ -0,0 +1,21 @@
+export default function AiMoodBar({ mood = 1 }) {
+  let color = "greenyellow",
+    label = "STABLE";
+  if (mood < 0.2) {
+    color = "salmon";
+    label = "DANGER";
+  } else if (mood < 0.4) {
+    color = "coral";
+    label = "WARNING";
+  } else if (mood < 0.6) {
+    color = "gold";
+    label = "CAUTION";
+  }
+  const width = `${Math.ceil(mood * 100)}%`;
+  return <div style={{
+    width: width,
+    height: '100%',
+    backgroundColor: color,
+    borderRadius: '0.25rem'
+  }}>{label}</div>;
+}
