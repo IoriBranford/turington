15e61541c487514e2c22df41434e4eb99c4056f0
IoriBranford <ioribranford@gmail.com>
2023-06-22 18:39:50 -0700

    Fix build error
    From trying to access nonexistent `window` during static rendering.

--- src/components/GameOverBlood.tsx
+++ src/components/GameOverBlood.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from "react";
+
 const BloodFiles = Array(7)
   .fill(0)
   .map((_, i) => `bloodsplats_000${i + 1}.png`);
 
 export default function GameOverBlood({ hidden = false }) {
+  const [positions, setPositions] = useState(Array(28).fill([0,0]))
+  useEffect(()=>{
+    setPositions(positions.map(()=> [
+      Math.random() * (window.innerWidth - 128),
+      Math.random() * (window.innerHeight - 128)
+    ]))
+  }, [])
+
   return (
     <>
-      {Array(28)
-        .fill(0)
-        .map((_, i) => {
+      {positions.map((position, i) => {
           const file = BloodFiles[i % BloodFiles.length];
           return (
             <img
@@ -18,8 +26,8 @@ export default function GameOverBlood({ hidden = false }) {
               draggable={false}
               style={{
                 position: "fixed",
-                left: `${Math.random() * (window.innerWidth - 128)}px`,
-                top: `${Math.random() * (window.innerHeight - 128)}px`,
+                left: `${position[0]}px`,
+                top: `${position[1]}px`,
               }}
             />
           );
